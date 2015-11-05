6f0074ea02db58c16f13c15588cc18ea090ef25b
tengai90 <z.smulko@siadrs.lv>
2015-11-05 09:42:46 +0200

    Task 4

--- Homework_2/Git_tutorial/Git_tutorial/Program.cs
+++ Homework_2/Git_tutorial/Git_tutorial/Program.cs
@@ -10,6 +10,14 @@ namespace Git_tutorial
     {
         static void Main(string[] args)
         {
+            Console.WriteLine("Enter x: ");
+            string x = Console.ReadLine();
+            Console.WriteLine("Enter y: ");
+            string y = Console.ReadLine();
+
+            Console.WriteLine("x: " + x + "  y: " + y);
+
+            Console.ReadLine();
         }
     }
 }
